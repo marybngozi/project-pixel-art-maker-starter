4f6273444224cab8b18301892e6ca8c37ae590fa
marybngozi <umeh.mary@appmartgroup.com>
2022-04-02 20:32:22 +0100

    pixel perfect

---
+++ Todo.md
@@ -0,0 +1,8 @@
+- [ ] Entering whole numbers in the "Height" and "Width" fields, and submitting the form, causes an empty grid to
+      appear on the page.
+
+- [ ] Choosing a color from the color selector and clicking on a grid square causes that grid square
+      (and only that square) to change color.
+- [ ] Event listeners are properly added to the grid squares (and not to the border or the table itself).
+
+- [ ] If there are already colored squares in the grid, clicking the Submit button clears them out.
--- designs.js
+++ designs.js
@@ -1,10 +1,51 @@
 // Select color input
-// Select size input
+const colorInput = document.querySelector("#colorPicker");
+let colorVal = colorInput.value;
 
 // When size is submitted by the user, call makeGrid()
+document.querySelector("#sizePicker").addEventListener("submit", (e) => {
+  e.preventDefault();
 
-function makeGrid() {
+  let gridHeight = e.target.inputHeight.value;
+  let gridWidth = e.target.inputWidth.value;
 
-// Your code goes here!
+  // validating the input and making sure they are whole numbers
+  gridHeight = isNaN(Number(gridHeight)) ? 0 : Math.round(Number(gridHeight));
+  gridWidth = isNaN(Number(gridWidth)) ? 0 : Math.round(Number(gridWidth));
 
+  // return if the any of the values are 0
+  if (!gridHeight || !gridWidth) {
+    return false;
+  }
+
+  makeGrid(gridHeight, gridWidth);
+});
+
+// event for the color input
+colorInput.addEventListener("input", (e) => {
+  e.preventDefault();
+
+  colorVal = e.target.value;
+});
+
+function makeGrid(height, width) {
+  // Your code goes here!
+  const tableEl = document.querySelector("#pixelCanvas");
+  tableEl.innerHTML = "";
+
+  // make a loop for the rows (tr) using the height
+  for (let i = 0; i < height; i++) {
+    const trEl = document.createElement("tr");
+
+    // make the loop for the columns (td) using the width
+    for (let k = 0; k < width; k++) {
+      const tdEl = document.createElement("td");
+      tdEl.addEventListener("click", (e) => {
+        tdEl.style.backgroundColor = colorVal;
+      });
+      trEl.appendChild(tdEl);
+    }
+
+    tableEl.appendChild(trEl);
+  }
 }
